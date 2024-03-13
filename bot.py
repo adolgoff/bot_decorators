76351d1db25234815db5884c2dc797b54fa939aa
Alexander Dolgov <a.dolgov@conundrum.ai>
2024-03-13 22:50:13 +0100

    Change to dynamic imports

--- bot.py
+++ bot.py
@@ -1,9 +1,18 @@
 from utils.action_decorator import ActionDecorator
-import handlers
+import os
+import importlib
 
 class Bot:
     def __init__(self):
         self.handlers = ActionDecorator.handlers
+        self.load_handlers()
+
+    def load_handlers(self):
+        handlers_dir = 'handlers'
+        for file in os.listdir(handlers_dir):
+            if file.endswith('.py') and not file.startswith('__'):
+                module_name = file[:-3]
+                importlib.import_module(f'{handlers_dir}.{module_name}')
 
     def handle_message(self, message):
         command = message.split()[0]
--- handlers/__init__.py
+++
@@ -1,3 +0,0 @@
-from .hello_handler import HelloHandler
-from .bye_handler import ByeHandler
-from .help_handler import HelpHandler
